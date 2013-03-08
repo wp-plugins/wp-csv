e3e5f918a3a8dc4e33a9281b83faefd1bb465347
cpkwebsolutions <cpkwebsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-08 02:55:18 +0000

    Tagging version 1.3.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-csv/tags/1.3.7@677866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpcsv.php
+++ wpcsv.php
@@ -3,7 +3,7 @@
 Plugin Name: WP CSV
 Plugin URI: http://cpkwebsolutions.com/plugins/wp-csv
 Description: A powerful, yet easy to use, CSV Importer/Exporter for Wordpress posts and pages. 
-Version: 1.3.6
+Version: 1.3.7
 Author: CPK Web Solutions
 Author URI: http://cpkwebsolutions.com
 
@@ -33,6 +33,7 @@ require_once( 'pws_wpcsv_engine.php' );
 // Global constants
 define( 'ERROR_MISSING_POST_ID', 1 );
 define( 'ERROR_MISSING_POST_PARENT', 2 );
+define( 'ERROR_INVALID_AUTHOR', 3 );
 
 // Initialise main class
 if ( !class_exists( 'pws_wpcsv' ) ) {
@@ -64,7 +65,7 @@ if ( !class_exists( 'pws_wpcsv' ) ) {
 			add_option( $this->option_name, $settings ); // Does nothing if already exists
 
 			$this->settings = get_option( $this->option_name );
-			$this->settings['version'] = '1.3.5';
+			$this->settings['version'] = '1.3.7';
 
 			$current_keys = array_keys( $this->settings );
 			foreach( array_keys( $settings ) as $key ) {
